7ab7f4eee4e2e843a92ea61cdb1fbdf6951ef9ac
kongnuy <kongnuy@avomark.fr>
2021-12-01 13:26:45 +0100

    Secure server

--- launch.php
+++ launch.php
@@ -3,10 +3,27 @@ require __DIR__ . '/vendor/autoload.php';
 
 use MyApp\MyServer;
 use Ratchet\Http\HttpServer;
+use Ratchet\MessageComponentInterface;
 use Ratchet\Server\IoServer;
 use Ratchet\WebSocket\WsServer;
+use React\EventLoop\Factory as LoopFactory;
+use React\Socket\SecureServer as SecureReactor;
+use React\Socket\Server as Reactor;
 
-$server = IoServer::factory(
+class IoSecureServer extends IoServer {
+  public static function factory(MessageComponentInterface $component, $port = 80, $address = '0.0.0.0', $secureOptions = []) {
+    $loop = LoopFactory::create();
+    $socket = new Reactor($address . ':' . $port, $loop);
+
+    if (!empty($secureOptions) && is_array($secureOptions)) {
+      $socket = new SecureReactor($socket, $loop, $secureOptions);
+    }
+
+    return new static($component, $socket, $loop);
+  }
+}
+
+$server = IoSecureServer::factory(
   new HttpServer(
     new WsServer(
       new MyServer()
